0356cb8d4410c0d388b8467772184890bb638d88
SimeonGerginov <srgerginov@uni-sofia.bg>
2018-08-27 21:01:16 +0300

    Added .gitignore file. Added FileManagement Web Api project.

---
+++ Server/Web/FileManagement.Web.Api/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace FileManagement.Web.Api.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ Server/Web/FileManagement.Web.Api/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace FileManagement.Web.Api.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
