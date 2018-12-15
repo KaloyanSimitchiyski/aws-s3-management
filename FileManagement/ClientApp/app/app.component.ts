4bb41ba906c2f42088d2ff86275105be3ce92432
SimeonGerginov <simeongerginov1@gmail.com>
2018-12-15 17:49:21 +0200

    Added shared module and navigation component

--- FileManagement/ClientApp/app/app.component.ts
+++ FileManagement/ClientApp/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root'
+  selector: 'app-root',
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'client-app';
 }
--- FileManagement/ClientApp/app/app.module.ts
+++ FileManagement/ClientApp/app/app.module.ts
@@ -3,17 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+import { SharedModule } from './shared/shared.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    NgbModule.forRoot()
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [ BrowserModule, NgbModule.forRoot(), SharedModule ],
+  declarations: [ AppComponent ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
