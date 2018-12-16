0887e5121fd7ce129760818c1f247f5df1e02a4d
SimeonGerginov <simeongerginov1@gmail.com>
2018-12-16 17:27:07 +0200

    Added Routing module

--- FileManagement/ClientApp/app/app.module.ts
+++ FileManagement/ClientApp/app/app.module.ts
@@ -3,12 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  imports: [ BrowserModule, NgbModule.forRoot(), SharedModule ],
+  imports: [
+    BrowserModule,
+    NgbModule.forRoot(),
+    AppRoutingModule,
+    SharedModule
+  ],
   declarations: [ AppComponent ],
   bootstrap: [ AppComponent ]
 })
